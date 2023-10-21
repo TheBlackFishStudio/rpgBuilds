006cbc0d7f592bc9e6ae59340a1471b2e9e8dc67
manuMunoz875 <theblackfishstudio@gmail.com>
2023-10-21 13:36:15 -0300

    Entrada 02
    Se agregan las variables y funciones en JS

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+let nuevoSection = document.createElement("section"); //Creo un select
+let article = document.createElement("article"); //Creo un select
+let header = document.createElement("header");
+let headDos = document.createElement("h2");
+
+function buildIni(){
+    console.log('test');
+    nuevoSection.appendChild(article);
+}
+function buildSelectGame(){
+    
+}
+
+function buildSelectClass(){
+       
+}
+function buildResult(){
+       
+}
